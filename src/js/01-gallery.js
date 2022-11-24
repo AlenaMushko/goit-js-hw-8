62abf709fc68410f3be99e8debc3c492b4559c99
alena <evstafienko_a@ukr.net>
2022-11-24 23:18:20 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+import galleryCardTpl from '../templates/gallery-card.hbs';
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.addEventListener("click", onClickCreateModalWindow);
+
+const itemEl = galleryItems.map(galleryCardTpl).join("");
+galleryEl.insertAdjacentHTML("beforeend", itemEl);
+
+function onClickCreateModalWindow(e) {
+  // забороняємо стандартні дії
+  e.preventDefault();
+  // вказую, що відкривати лише img
+  const isIMGEl = e.target.nodeName;
+  if (isIMGEl !== "IMG") {
+    return;
+  }
+}
+  // підключаємо бібліотеку SimpleLightbox
+ const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+    captionsData: "alt",
+    captionsDelay: 250,
+  })
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
+ 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
