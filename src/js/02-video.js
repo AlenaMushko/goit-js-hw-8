7ec1779234335bbbd19864fdbffdb99bc226b134
alena <evstafienko_a@ukr.net>
2022-11-25 02:50:36 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const STORAGE_KEY = 'videoplayer-current-time';
-const player = new Player(iframe);
+// const iframe = document.querySelector('iframe');
+// const STORAGE_KEY = 'videoplayer-current-time';
+// const player = new Player(iframe);
  
-player.on('timeupdate', throttle(onPlay, 1000));
+// player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-  localStorage.setItem(STORAGE_KEY, seconds);
-}
+// function onPlay({ seconds }) {
+//   localStorage.setItem(STORAGE_KEY, seconds);
+// }
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
