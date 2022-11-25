5e7b16aac77cb07a429d883ab5e00e0e716f18cc
alena <evstafienko_a@ukr.net>
2022-11-26 00:20:39 +0200

    3.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,3 @@
-
 import throttle from "lodash.throttle";
 
 const refs = {
@@ -8,47 +7,47 @@ const STORAGE_KEY = 'feedback-form-state';
 // зберігаємо імейл та повідомлення в обєкт
 let formData = {};
 
-refs.formEl.addEventListener('submit', onFormSubmit);
-refs.formEl.addEventListener('input', throttle(onFormInput, 500));
+// refs.formEl.addEventListener('submit', onFormSubmit);
+// refs.formEl.addEventListener('input', throttle(onFormInput, 500));
 
 populateTextarea();
 
-function onFormInput(e){
-  // отримуємо значення
-const message = e.target.value;
-  // зберігаємо його в сховище    
-  localStorage.setItem(STORAGE_KEY, message )
-}
+// function onFormInput(e){
+//   // отримуємо значення
+// const message = e.target.value;
+//   // зберігаємо його в сховище    
+//   localStorage.setItem(STORAGE_KEY, message )
+// }
 
-function onFormSubmit(e){
-  // Х поведінку по замовчуванню
-e.preventDefault();
-// Забираємо повідомлення із сховища і чистимо форму, reset() для очистки форм
-e.currentTarget.reset();
-const { email, message, } = e.currentTarget.elements;
-console.log(e.currentTarget.element);
-if( refs.emailEl.value === '' || refs.textareaEl.value === ''){
-   alert(`Заповніть всі поля`);
-   return;
-}
-  localStorage.removeItem(STORAGE_KEY); 
- // Форма відправляється при заповнених 2-х полях форми
- console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-}
+// function onFormSubmit(e){
+//   // Х поведінку по замовчуванню
+// e.preventDefault();
+// // Забираємо повідомлення із сховища і чистимо форму, reset() для очистки форм
+// e.currentTarget.reset();
+// const { email, message, } = e.currentTarget.elements;
+// console.log(e.currentTarget.element);
+// if( refs.emailEl.value === '' || refs.textareaEl.value === ''){
+//    alert(`Заповніть всі поля`);
+//    return;
+// }
+//   localStorage.removeItem(STORAGE_KEY); 
+//  // Форма відправляється при заповнених 2-х полях форми
+//  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+// }
 
 function populateTextarea(){
   // при перезагрузці форми зберігається повідомлення, якщо воно не було відправлене
 const savadMessage = localStorage.getItem(STORAGE_KEY);
   // якщо вже є відгук, то його записуємо в localStorage
 if (savadMessage){
-   refs.textareaEl.value = savadMessage;
+   refs.formEl.value = savadMessage;
 }
 }
 
-function onFormOrder(e){
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// function onFormOrder(e){
+//   formData[e.target.name] = e.target.value;
+//   console.log(formData);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
   
   
-}
+// }
