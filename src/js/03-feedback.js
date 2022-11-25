af7108e94786a42efb5c7792b244b77e3768ec0f
alena <evstafienko_a@ukr.net>
2022-11-26 01:28:51 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,50 +4,40 @@ const refs = {
   formEl: document.querySelector('.feedback-form'),
   }
 const STORAGE_KEY = 'feedback-form-state';
+
 // зберігаємо імейл та повідомлення в обєкт
-let formData = {};
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+onPopulateForm()
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onFormInput, 500));
 
-// refs.formEl.addEventListener('submit', onFormSubmit);
-// refs.formEl.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+// отримуємо значення із форми
+formData[e.target.name] = e.target.value;
+// записуємо значення в localStorage
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-populateTextarea();
-
-// function onFormInput(e){
-//   // отримуємо значення
-// const message = e.target.value;
-//   // зберігаємо його в сховище    
-//   localStorage.setItem(STORAGE_KEY, message )
-// }
-
-// function onFormSubmit(e){
-//   // Х поведінку по замовчуванню
-// e.preventDefault();
-// // Забираємо повідомлення із сховища і чистимо форму, reset() для очистки форм
-// e.currentTarget.reset();
-// const { email, message, } = e.currentTarget.elements;
-// console.log(e.currentTarget.element);
-// if( refs.emailEl.value === '' || refs.textareaEl.value === ''){
-//    alert(`Заповніть всі поля`);
-//    return;
-// }
-//   localStorage.removeItem(STORAGE_KEY); 
-//  // Форма відправляється при заповнених 2-х полях форми
-//  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-// }
-
-function populateTextarea(){
-  // при перезагрузці форми зберігається повідомлення, якщо воно не було відправлене
-const savadMessage = localStorage.getItem(STORAGE_KEY);
+function onPopulateForm(){
+  const savedInputForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
   // якщо вже є відгук, то його записуємо в localStorage
-if (savadMessage){
-   refs.formEl.value = savadMessage;
-}
+  if(savedInputForm){
+   refs.formEl.email.value = savedInputForm.email;
+   refs.formEl.message.value = savedInputForm.message;
+  }
 }
 
-// function onFormOrder(e){
-//   formData[e.target.name] = e.target.value;
-//   console.log(formData);
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  
-  
-// }
+function onFormSubmit(e){
+  // забороняється поведінка по замовчуванню 
+  e.preventDefault();
+
+  const { email, message} = e.currentTarget.elements;
+if(email.value === '' || message.value === ''){
+alert( `Заповніть, будь ласка, всі поля!`)
+}
+// чистимо форму
+e.currentTarget.reset();
+console.log(formData);
+formData = {};
+localStorage.removeItem(STORAGE_KEY);
+}
