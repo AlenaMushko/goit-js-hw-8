89b8e601cf8c535fec84e6d26551be956a4cdfb4
alena <evstafienko_a@ukr.net>
2022-11-25 23:13:37 +0200

    3.0

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,9 +3,7 @@ import throttle from "lodash.throttle";
 
 const refs = {
   formEl: document.querySelector('.feedback-form'),
-  textareaEl: document.querySelector('.feedback-form textarea'),
-  emailEl: document.querySelector('.feedback-form input'),
-}
+  }
 const STORAGE_KEY = 'feedback-form-state';
 // зберігаємо імейл та повідомлення в обєкт
 let formData = {};
@@ -13,25 +11,30 @@ let formData = {};
 refs.formEl.addEventListener('submit', onFormSubmit);
 refs.formEl.addEventListener('input', throttle(onFormInput, 500));
 
+populateTextarea();
+
 function onFormInput(e){
   // отримуємо значення
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
-  // зберігаємо його в сховище    JSON.stringify(formData)
-  localStorage.setItem(STORAGE_KEY, formData )
+const message = e.target.value;
+  // зберігаємо його в сховище    
+  localStorage.setItem(STORAGE_KEY, message )
 }
 
- function onFormSubmit(e){
+function onFormSubmit(e){
   // Х поведінку по замовчуванню
 e.preventDefault();
 // Забираємо повідомлення із сховища і чистимо форму, reset() для очистки форм
 e.currentTarget.reset();
+const { email, message, } = e.currentTarget.elements;
+console.log(e.currentTarget.element);
+if( refs.emailEl.value === '' || refs.textareaEl.value === ''){
+   alert(`Заповніть всі поля`);
+   return;
+}
   localStorage.removeItem(STORAGE_KEY); 
  // Форма відправляється при заповнених 2-х полях форми
-   console.log(localStorage.getItem(JSON.parse(formData)));
- 
+ console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
-
 
 function populateTextarea(){
   // при перезагрузці форми зберігається повідомлення, якщо воно не було відправлене
@@ -41,4 +44,11 @@ if (savadMessage){
    refs.textareaEl.value = savadMessage;
 }
 }
-populateTextarea();
+
+function onFormOrder(e){
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  
+  
+}
